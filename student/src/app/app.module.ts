e4cc4e6581843f09a671b3d2bca4cee57eabd08f
Ian Schmoel <ischmoel4391@fs.local>
2017-12-21 15:55:47 -0700

    added providers to module

--- student/src/app/app.module.ts
+++ student/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranscriptService } from './shared/transcript.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TranscriptService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
